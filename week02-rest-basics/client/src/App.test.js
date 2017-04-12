b02fa195f0802e7bf592d0d4f414cf11bad835b9
Kristi <knasu12@gmail.com>
2017-04-11 17:15:09 -0700

    week02-rest-basics

--- week02-rest-basics/client/src/App.test.js
+++ week02-rest-basics/client/src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('My rest basic test', function () {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+    });
+    it('expects true to be true', function () {
+        expect(true).toBe(true);
+    });
+
+    it('renders and reads H1 text', () => {
+        const  wrapper = shallow(<App/>);
+        const welcome = <h2>Welcome to React</h2>;
+        expect(wrapper.contains(welcome)).toEqual(true);
+    });
 });
--- week02-rest-basics/server/routes/api.js
+++ week02-rest-basics/server/routes/api.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/foo', function(request, response, next) {
-    var message = { 'result': 'success', 'foo': 'bar', 'file': 'api.js' };
+    var message = {
+        'result': 'success',
+        'foo': 'bar',
+        'file':'api.js' };
     console.log('Foo called on server with message:', message);
     response.send(message);
 });
