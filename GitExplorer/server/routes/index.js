157d0791c4e223a4def2f08877de819d24a02c40
Kristi <knasu12@gmail.com>
2017-05-09 12:40:24 -0700

    ConvertJsonToFieldDefinitions

--- GitExplorer/server/routes/index.js
+++ GitExplorer/server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var debug = require('debug')('git-convert');
+debug('this is a test');
 /* GET home page. */
 router.get('/', function(req, res, next) { 'use strict';
   res.render('index', { title: 'server' });
