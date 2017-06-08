1928fe5c4fb266d33524649adee8cbbe79d0778c
Kristi <knasu12@gmail.com>
2017-06-08 15:28:17 -0700

    redux basics

--- redux-simple-inclass/src/index.js
+++ redux-simple-inclass/src/index.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import FakeRedux from './FakeRedux';
+import SimpleRedux from './SimpleRedux';
+import AppConnect from './AppConnect';
+import AppConnectMaps from './AppConnectMaps';
+import DispatchConnect from './DispatchConnect';
+import DispactchComponentConnect from './DispatchComponentConnect';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import spokesman from './spokesman';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
-import AppNoProps from './AppNoProps';
-import AppConnect from './AppConnect';
-import Connector from './Connector';
-import DispatchConnect from './DispatchConnect';
+let store = createStore(spokesman);
 
-
-let store = createStore(spokesman)
 ReactDOM.render(
-   <div>
+    <div>
         <Provider store={store}>
-        <div>
-            <App store={store}/>
-            <hr/><hr/>
-            <AppNoProps/>
-            <hr/><hr/>
-            <DispatchConnect/>
-            <hr/><hr/>
-            <AppConnect/>
-            <Connector/>
-            <hr/><hr/>
-            <FakeRedux/>
-        </div>
+            <div>
+                <AppConnect/>
+                <hr /> <hr />
+                <AppConnectMaps/>
+                <hr /> <hr />
+                <DispactchComponentConnect/>
+                <hr /> <hr />
+                <DispatchConnect/>
+                <hr /> <hr />
+                <App store={store}/>
+                <hr /> <hr />
+                <SimpleRedux />
+            </div>
         </Provider>
-   </div>,
-
+    </div>,
     document.getElementById('root'));
 registerServiceWorker();
--- redux-simple-inclass/src/spokesman.js
+++ redux-simple-inclass/src/spokesman.js
@@ -1,14 +1,20 @@
-/**
- * Created by bcuser on 6/1/17.
- */
 const spokesman = (state = { statement: 'No comment' }, action) => {
     switch (action.type) {
         case 'VERIFY':
-            return { statement: 'We stand by it. In fact, we invented it.' };
+            return {
+                statement: 'We stand by it. In fact, we invented it.',
+                kind: 'verify'
+            };
         case 'DENY':
-            return { statement: 'We deny everything. We have never heard of it.' };
+            return {
+                statement: 'We deny everything. We have never heard of it.',
+                kind: 'deny'
+            };
         case 'NO COMMENT':
-            return {statement: 'No comment.'};
+            return {
+                statement: 'No comment.',
+                kind: 'no Comment'
+            };
         default:
             return state;
     }
